b13312224d8ae4ec9e922d49c7eacbbd30a720b3
Pratham Shah <prathamshah.akkomplish@gmail.com>
2025-07-14 16:20:33 +0530

    test

--- rendom.py
+++ rendom.py
@@ -2,3 +2,5 @@ import random
 print(f"Random number between 1 and 100: {random.randint(1, 100)}")
 fruits = ['apple', 'banana', 'orange', 'grape']
 print(f"Today's lucky fruit is: {random.choice(fruits).upper()}")
+
+
